eaff95d0752dc4a1c3218af77b7f79a11dd02580
289222346 <289222346@qq.com>
2020-08-08 11:02:43 +0800

    将Web项目分离出去

--- web/src/main/resources/static/js/util/head.js
+++
@@ -1,9 +0,0 @@
-//加载jquery
-document.writeln("<meta charset=\"utf-8\">");
-//加载jquery
-document.writeln("<script src=\"/web/jqery/jquery-1.7.2.min.js\"></script>");
-//数据绑定
-document.writeln("<script src=\"/web/js/util/DataBind.js\"></script>");
-//公共服务
-document.writeln("<script src=\"/web/js/util/BaseController.js\"></script>");
-
